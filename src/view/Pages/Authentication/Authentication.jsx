62d847613ef5d3ba3304dae4e2414f44b56f4879
Jan Skrzynski <janskrzy@student.arteveldehs.be>
2024-08-16 19:36:18 +0200

    Final

--- src/view/Pages/Authentication/Authentication.jsx
+++ src/view/Pages/Authentication/Authentication.jsx
@@ -11,6 +11,8 @@ import ActivityPage from "../Activity/ActivityPage";
 import Main from "../Main/Main";
 import Add from "../Add/Add";
 import Components from "../Components/Components";
+import AddPage from "../../components/AddPage/AddPage";
+import DetailPage from "../../components/DetailPage/DetailPage";
 
 const Authentication = () => {
   return (
@@ -19,12 +21,16 @@ const Authentication = () => {
         <Route path={ROUTES.home} element={<Home />} />
         <Route path={ROUTES.main} element={<Main />} />
         <Route path={ROUTES.add} element={<Add />} />
-        <Route path={ROUTES.components} element={<Components />} />
-        <Route path={ROUTES.icon} element={<IconPage />} />
-        <Route path={ROUTES.button} element={<ButtonPage />} />
-        <Route path={ROUTES.tag} element={<TagPage />} />
-        <Route path={ROUTES.formField} element={<FormFieldPage />} />
-        <Route path={ROUTES.activity} element={<ActivityPage />} />
+        <Route path={ROUTES.components.main} element={<Components />} />
+
+        <Route path={ROUTES.components.activity} element={<ActivityPage />} />
+        <Route path={ROUTES.components.addPage} element={<AddPage />} />
+        <Route path={ROUTES.components.button} element={<ButtonPage />} />
+        <Route path={ROUTES.components.detailPage} element={<DetailPage />} />
+        <Route path={ROUTES.components.icon} element={<IconPage />} />
+        <Route path={ROUTES.components.tag} element={<TagPage />} />
+        <Route path={ROUTES.components.formField} element={<FormFieldPage />} />
+
         <Route path={ROUTES.notFound} element={<NotFound />} />
       </Routes>
     </>
--- src/view/Pages/Components/Components.jsx
+++ src/view/Pages/Components/Components.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import style from "./Components.module.css";
+import ROUTES from "../../../consts/Routes";
+import { Link } from "react-router-dom";
 
 const Components = () => {
   return (
     <>
       <aside>
-        <nav></nav>
+        <nav>
+          <ul>
+            <li>
+              <Link to={ROUTES.activity}>Activity</Link>
+            </li>
+          </ul>
+        </nav>
       </aside>
+      <main></main>
     </>
   );
 };
